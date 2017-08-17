fe421cedccb576e655f6ac3689f9840dfa744e14
Marc-Antoine <marc@rvit.co>
2017-08-16 22:01:53 -0400

    Implement role choosing interaction

--- staticfiles/js/modelmaker.js
+++ staticfiles/js/modelmaker.js
@@ -1,27 +1,30 @@
+var variable_names = [
+	"studyid",
+	"subjid",
+	"siteid",
+	"sex",
+	"agedays",
+	"WHZ",
+	"region",
+	"risk factor"
+];
+
+var variables = variable_names.map(function (variable_name) {
+	return {
+		name: variable_name,
+		role: "W"
+	}
+});
+
 var app = new Vue({
   delimiters: ['${', '}'],
   el: '#modelmaker',
   data: {
+  	roles: ['W', 'A', 'Y'],
     message: 'Hello Vue!',
-	variables: [
-		{
-			name: "studyid",
-			role: "W",
-			isW: true,
-		},
-		{
-			name: "subjid",
-			role: "A",
-			isW: true,
-		},
-		{
-			name: "siteid",
-			role: "Y",
-			isW: true,
-		},
-	]
+	variables: variables
   },
   computed: {
 
   }
-})
+});
