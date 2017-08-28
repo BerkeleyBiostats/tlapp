e712800cf53ddebbd9456f723e6bfebf006146dc
Marc-Antoine <marc@rvit.co>
2017-08-27 18:01:46 -0700

    Add model template data to devseed

--- core/management/commands/devseed.py
+++ core/management/commands/devseed.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
+from core import models
 
 class Command(BaseCommand):
     help = "Initialize the app for development"
@@ -8,3 +9,31 @@ class Command(BaseCommand):
         user = User.objects.create_superuser(
             'admin', 'admin@example.com', 'admin')
         user.save()
+
+        parameters = {
+            "parameters": [{
+                "name": "ABar",
+                "type": "float",
+                "help": "A block of help text that explains the model input.",
+            }, {
+                "name": "Learners",
+                "type": "enum",
+                "choices": [
+                    "GLM",
+                    "Random Forest",
+                    "Regression"
+                ],
+            },]
+        }
+
+        code = """
+            print('hello world')
+        """
+
+        mt = models.ModelTemplate(
+            name='HGCKki_sample.R',
+            parameters=parameters,
+            code=code
+        )   
+        mt.save()
+
