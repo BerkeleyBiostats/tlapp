68a02e5e23e1b1d1773be7d09e4ed8a16d811279
Marc-Antoine <marc@rvit.co>
2017-09-04 22:07:35 -0700

    Implement worker running the new type of job

--- core/tasks.py
+++ core/tasks.py
@@ -3,6 +3,7 @@ import tempfile
 import os
 import subprocess
 import traceback
+import json
 from core import models
 
 def handle_jobs():
@@ -19,16 +20,39 @@ def handle_jobs():
 
     try:
         code_folder = tempfile.mkdtemp()
+        output_folder = tempfile.mkdtemp()
 
         print(code_folder)
+        print("Outputs %s" % output_folder)
 
         script_name = 'script.R'
         code_filename = os.path.join(code_folder, script_name)
         print(code_filename)
         with open(code_filename, 'w') as code_file:
             code_file.write(job.model_template.code)
+
+        input_name = 'inputs.json'
+        input_filename = os.path.join(code_folder, input_name)
+        print(input_name)
+
+        parameters = job.model_template.parameters
+        parameters['output_directory'] = output_folder
+
+        with open(input_filename, 'w') as input_file:
+            input_file.write(json.dumps(job.model_template.parameters))
         
-        script_resp = subprocess.check_output(["Rscript", "--default-packages=methods,stats", code_filename])
+        rpath = '/usr/local/lib/R/site-library/tltools/'
+        cmd = [
+            "Rscript",
+            "--default-packages=methods,stats",
+            rpath + "scripts/run_job.R",
+            code_filename,
+            input_filename,
+        ]
+
+        print(" ".join(cmd))
+
+        script_resp = subprocess.check_output(cmd)
 
         job.output = script_resp
         job.status = models.ModelRun.status_choices['success']
