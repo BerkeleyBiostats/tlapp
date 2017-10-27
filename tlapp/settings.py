bb0ad569e97d4fe6b6eb8f2dde8cd9c8a4f949eb
Marc-Antoine <marc@rvit.co>
2017-10-26 20:23:52 -0700

    Try sendgrid instead

--- tlapp/settings.py
+++ tlapp/settings.py
@@ -135,12 +135,8 @@ STATICFILES_DIRS = (
 )
 
 if os.environ.get("MAILGUN_API_KEY"):
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = os.environ.get('MAILGUN_SMTP_SERVER')
-    EMAIL_PORT = os.environ.get('MAILGUN_SMTP_PORT')
-    EMAIL_HOST_USER = os.environ.get('MAILGUN_SMTP_LOGIN')
-    EMAIL_HOST_PASSWORD = os.environ.get('MAILGUN_SMTP_PASSWORD')
-    EMAIL_USE_TLS = True
+    EMAIL_BACKEND = 'sgbackend.SendGridBackend'
+    SENDGRID_API_KEY = os.environ.get('SENDGRID_API_KEY')
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
