39e2128748ec5b69eeef3a5a5e1390f5f6521f78
Marc-Antoine <marc@rvit.co>
2018-12-13 11:43:28 -0800

    Fix the broken modelrun admin page

--- core/admin.py
+++ core/admin.py
@@ -7,5 +7,31 @@ from . import models
 # Register your models here.
 admin.site.register(models.AnalysisTemplate)
 admin.site.register(models.Dataset)
-admin.site.register(models.ModelRun)
 admin.site.register(models.Token)
+
+
+@admin.register(models.ModelRun)
+class ModelRunAdmin(admin.ModelAdmin):
+	fields = (
+		"created_by",
+		"status",
+		"backend",
+		"ghap_username",
+		"ghap_ip",
+		"base_url",
+		"title",
+		"output_url",
+		"traceback",
+		"model_template",
+		"dataset",
+		"postprocessing_attempts",
+		"postprocessing_attempted_at",
+		"postprocessing_traceback",
+		"is_batch",
+		"last_heartbeat",
+		"inputs",
+		"code",
+		"provision",
+	)
+
+# admin.site.register(models.ModelRun)
